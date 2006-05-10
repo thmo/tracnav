1e0ea9111ae242c244997a820bb3487657c56a94
Thomas Moschny <thomas.moschny@gmx.de>
2006-05-10 12:21:26 +0000

    Add a specific style for IE6 that doesn't use position:relative. Should fix #257 and #258.
    svn path=/trac/plugins/tracnav/; revision=3095

--- tracnav/tracnav.py
+++ tracnav/tracnav.py
@@ -55,7 +55,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 """
 __id__        = '$Id$'
-__version__   = '3.92pre3'
+__version__   = '3.92pre4'
 __revision__  = '$LastChangedRevision$'
 
 import re
