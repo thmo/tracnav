7a387ac0232b4dc5861455de372981423ff865d4
Thomas Moschny <thomas.moschny@gmx.de>
2007-03-02 15:22:36 +0000

    Propagate patch from r3170.
    svn path=/trac/plugins/tracnav-0.11/; revision=3171

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 
-from setuptools import setup
 from tracnav import __version__ as VERSION
+from setuptools import setup
+
+# workaround for a setuptools bug: sdist doesn't honor package_data
+from setuptools.command.sdist import sdist as _sdist
+import glob, os
+
+class mysdist(_sdist):
+
+    def make_distribution(self):
+        # add the package_data files to self.filelist
+        for key, globs in self.distribution.package_data.iteritems(): 
+            for pattern in globs:
+                self.filelist.extend(glob.glob(os.path.join(key, pattern)))
+        _sdist.make_distribution(self)
+
 
 setup(
     name = 'TracNav',
@@ -13,8 +27,9 @@ setup(
     maintainer = 'Thomas Moschny',
     maintainer_email = 'moschny@ipd.uni-karlsruhe.de',
     url = 'http://svn.ipd.uka.de/trac/javaparty/wiki/TracNav',
-    description = 'The navigation bar for Trac',
+    description = 'The Navigation Bar for Trac',
     entry_points={'trac.plugins': ['TracNav = tracnav.tracnav']},
     keywords = 'trac toc',
     license = 'GPL',
+    cmdclass = { 'sdist' : mysdist }
 )
