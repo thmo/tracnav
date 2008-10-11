40b7490e00691482c14e716b333da7047be203c7
Thomas Moschny <thomas.moschny@gmx.de>
2008-10-11 14:02:05 +0000

    Remove workaround for sdist bug.
    svn path=/trac/plugins/tracnav-0.11/; revision=3260

--- setup.py
+++ setup.py
@@ -3,20 +3,6 @@
 from tracnav import __version__ as VERSION
 from setuptools import setup
 
-# workaround for a setuptools bug: sdist doesn't honor package_data
-from setuptools.command.sdist import sdist as _sdist
-import glob, os
-
-class mysdist(_sdist):
-
-    def make_distribution(self):
-        # add the package_data files to self.filelist
-        for key, globs in self.distribution.package_data.iteritems(): 
-            for pattern in globs:
-                self.filelist.extend(glob.glob(os.path.join(key, pattern)))
-        _sdist.make_distribution(self)
-
-
 setup(
     name = 'TracNav',
     version = VERSION,
@@ -31,5 +17,4 @@ setup(
     entry_points={'trac.plugins': ['TracNav = tracnav.tracnav']},
     keywords = 'trac toc',
     license = 'GPL',
-    cmdclass = { 'sdist' : mysdist }
 )
