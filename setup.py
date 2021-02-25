acc8e6bba55b62ead1de4221da9e3c61558ffd4a
Thomas Moschny <thomas.moschny@gmx.de>
2021-02-25 19:41:06 +0100

    Move to PEP517 style packaging.

--- setup.py
+++ setup.py
@@ -2,30 +2,4 @@
 
 from setuptools import setup
 
-setup(
-    name='TracNav',
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    packages=['tracnav'],
-    package_data={'tracnav': ['htdocs/css/*.css']},
-    author='Bernhard Haumacher',
-    author_email='haui@haumacher.de',
-    maintainer='Thomas Moschny',
-    maintainer_email='thomas.moschny@gmx.de',
-    url='https://svn.ipd.kit.edu/trac/javaparty/wiki/TracNav',
-    download_url='https://svn.ipd.kit.edu/trac/javaparty/wiki/TracNav/DownloadAndInstall',
-    description='The Navigation Bar for Trac',
-    entry_points={'trac.plugins': ['TracNav = tracnav.tracnav']},
-    keywords='trac toc plugin',
-    license='GPLv2+',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Trac',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
-)
+setup()
